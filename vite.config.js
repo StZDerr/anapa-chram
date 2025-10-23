4d4494628a9f17176f8b8d0afdcf8a5ef4c0ba47
StZDer <Sasha-vasilchenko-1999@mail.ru>
2025-10-23 14:22:45 +0300

    Обновлен vite.config.js: добавлены все CSS и JS для сборки

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
         laravel({
             input: [
                 "resources/sass/app.scss",
-                "resources/js/app.js",
                 "resources/css/app.css",
                 "resources/css/welcome.css",
                 "resources/css/zapiski.css",
